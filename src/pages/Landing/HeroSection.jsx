4d3086f67bf3a4f5806639534e97133b93fc3a7d
m1rkk <mrakkus15@gmail.com>
2026-01-29 13:29:11 +0200

    feat: added gradient line in hero page fix: fixed responsivness by reducing size of objects

--- src/pages/Landing/HeroSection.jsx
+++ src/pages/Landing/HeroSection.jsx
@@ -3,11 +3,13 @@ import HomeButton from "@/shared/ui/HomeButton";
 
 export default function HeroSection(props) {
     return (
-    <div className="w-[94%] w-max-screen px-5 bg-contain bg-no-repeat bg-center aspect-[23/10] mt-20"
+    <div className="w-[94%] w-max-screen m-0 bg-contain bg-no-repeat bg-center aspect-[23/10] mt-20"
          style={{ backgroundImage: `url(${BgRectangle})` }}>
                 <HomeButton></HomeButton>
                 <h1 className="text-8xl relative left-1/6 bottom-1/6 font-inter font-medium text-[#638B9D] w-1/3">ClassMate</h1>
-            <img src="src/assets/imgs/calendarImg.png" alt="" className="w-3/10 relative left-260 bottom-1/2" />
+            <img src="src/assets/imgs/calendarImg.png" alt="" className="w-3/13 relative left-4/5 bottom-1/2 z-2" />
+        <div className="w-full h-1.5 bg-linear-to-r from-[#D0A4FA] to-[#A1E3F1] relative bottom-3/5 z-1"></div>
+
     </div>
 
     )
