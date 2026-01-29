5f2c6afe2fe76a1956086dbf9a5f2db19239bcc8
m1rkk <mrakkus15@gmail.com>
2026-01-29 19:44:55 +0200

    fix: size of the calendar img fixed to planned with remained responsiveness

--- src/pages/Landing/HeroSection.jsx
+++ src/pages/Landing/HeroSection.jsx
@@ -7,8 +7,9 @@ export default function HeroSection(props) {
          style={{ backgroundImage: `url(${BgRectangle})` }}>
                 <HomeButton></HomeButton>
                 <h1 className="text-8xl relative left-1/6 bottom-1/6 font-inter font-medium text-[#638B9D] w-1/3">ClassMate</h1>
-            <img src="src/assets/imgs/calendarImg.png" alt="" className="w-3/13 relative left-4/5 bottom-1/2 z-2" />
-        <div className="w-full h-1.5 bg-linear-to-r from-[#D0A4FA] to-[#A1E3F1] relative bottom-3/5 z-1"></div>
+            <img src="src/assets/imgs/calendarImg.png" alt="" className="w-4/14 relative left-8/11 bottom-1/2 z-2" />
+
+        <div className="w-full h-1 bg-linear-to-r from-[#D0A4FA] to-[#A1E3F1] relative bottom-11/18 z-1"></div>
 
     </div>
 
