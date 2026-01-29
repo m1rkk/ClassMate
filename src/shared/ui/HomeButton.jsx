eaa634ddbeb84a1503e846ac659203dad0703f34
m1rkk <mrakkus15@gmail.com>
2026-01-29 21:07:47 +0200

    fix: fixed responsiveness by using absolute feat: new text block added

--- src/shared/ui/HomeButton.jsx
+++ src/shared/ui/HomeButton.jsx
@@ -4,7 +4,7 @@ import homeIcon from "@/assets/imgs/material-symbols_home-outline-rounded.png"
 export default function HomeButton(){
     return (
         <button className="flex items-center justify-center w-3/27 bg-white rounded-2xl
-        h-3/14 left-1/70 relative">
+        h-3/14 left-1/70 absolute">
             <img src="src/assets/imgs/material-symbols_home-outline-rounded.png"
                  alt="" className="w-1/4"/></button>
     )
