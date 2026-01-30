72bcb0449617f71ccbc7d571ee1aa25cc6f1bf61
m1rkk <mrakkus15@gmail.com>
2026-01-30 19:23:23 +0200

    feat: "ABOUT" button added to hero

--- src/pages/Landing/HeroSection.jsx
+++ src/pages/Landing/HeroSection.jsx
@@ -2,7 +2,7 @@ import BgRectangle from '@/assets/imgs/heroBackgroundRectangle.png';
 import HomeButton from "@/shared/ui/HomeButton";
 import SquaredText from "@/shared/ui/SquaredTextElem";
 import InfoContainer from "@/shared/ui/InfoContainer";
-
+import AboutButton from "@/shared/ui/AboutButton";
 export default function HeroSection(props) {
     return (
     <div className="w-[94%] relative w-max-screen bg-contain bg-no-repeat bg-center aspect-[23/10] mt-20"
@@ -15,6 +15,7 @@ export default function HeroSection(props) {
 
         <SquaredText></SquaredText>
         <InfoContainer></InfoContainer>
+        <AboutButton></AboutButton>
     </div>
 
     )
