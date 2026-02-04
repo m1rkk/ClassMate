efb3c2dada3dc9cb41106fa64fd111caf5dda926
--replace-all <mrakkus15@gmail.com>
2026-02-04 23:23:05 +0200

    minor fixes in responsiveness still in work

--- src/pages/Landing/HeroSection.jsx
+++ src/pages/Landing/HeroSection.jsx
@@ -29,7 +29,7 @@ export default function HeroSection(props) {
                 style={{position:"absolute", top:"31%",left:"4.5%",zIndex:2}}>
                 <div className="relative text-[1.25rem] font-light w-32 h-2 flex items-center justify-center">Pieslegties</div>
             </LiquidGlass>
-            <button className={`bg-black pt-4 pb-[0.9rem] pr-7 p-7 rounded-[100px] absolute top-[29%] left-[9%] text-white z-1 text-[1.15rem] font-extralight`}>Registreties</button>
+            <button className={`bg-black pt-4 pb-[0.9rem] pr-7 p-7 rounded-[100px] absolute top-[29%] left-[9%] text-white z-10 text-[1.15rem] font-extralight`}>Registreties</button>
 
        <LiquidGlass
            displacementScale={170}
@@ -39,12 +39,17 @@ export default function HeroSection(props) {
                     blurAmount={0.0}
                     elasticity={0}
                     cornerRadius={18}
-                    style={{position:"absolute", top:"40%",left:"12%", zIndex:"2"}}>
-        <div className="relative w-130 h-30 text-white text-[1.25rem] font-light flex items-center justify-center">
+           style={{position:"absolute", top:"40%",left:"12%", zIndex:2}}
+       className={` lg:max-lg:ml-[7%] md:ml-[6%] sm:ml-[7%]`}>
+        <div className=" 2xl:w-130 xl:w-100 lg:w-70 md:w-40 sm:w-10
+        2xl:h-30 xl:h-25 lg:h-20 md:h-15 sm:h-10
+        text-white
+        xl:text-[1.25rem] lg:text-[1rem] md:text-[0.75rem] sm:text-[0.5rem]
+        font-light flex items-center justify-center">
             Mūsu platforma darbojas pēc līdzīga principa kā Airbnb, tikai paredzēta privāto skolotāju un instruktoru rezervācijām. Mūsu platforma darbojas pēc līdzīga principa kā Airbnb, tikai paredzēta privāto skolotāju un instruktoru rezervācijām..
         </div>
        </LiquidGlass>
-        <h1 className="text-white text-[7rem] font-extrabold absolute top-[45%] left-[1%] z-1 w-[26%] font-[Orbitron]">
+        <h1 className="text-white text-[7rem] font-extrabold absolute top-[45%] left-[1%] z-10 w-[26%] font-[Orbitron]">
             NEW WAY
             TO
             SCHEDULE
