fad7d5cc11cc424ebb21cb49b3ae042246a57dc9
--replace-all <mrakkus15@gmail.com>
2026-02-22 15:12:32 +0200

    basic routing for auth api created

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,14 +1,12 @@
 <?php
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Route;
 
-    Route::get('/pong', function () {
-        return response()->json(['message' => 'pong']);
-    });
-    Route::get('/me', function (Request $request) {
-        return response()->json([
-            'message' => 'ok',
-            'user' => $request->user(),
-        ]);
-    })->middleware('auth:sanctum');
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+    Route::prefix('auth')->group(function () {
+        Route::post('login', [AuthController::class, 'login']);  //login route kotorij svazivaetsa s login funkcijei v AuthController
+        Route::post('register', [AuthController::class, 'register']); //register route kotorij svazivaetsa s register funkcijei v AuthController
+        Route::post('me', [AuthController::class, 'me']); //me route kotorij svazivaetsa s me funkcijei v AuthController poka chto chisto zatichka
+    })
 ?>
