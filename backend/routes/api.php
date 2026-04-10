1a48727b15f967557078aecdfe23751f4cafd58a
--replace-all <mrakkus15@gmail.com>
2026-04-10 11:41:29 +0300

    feat: added lesson endpoints and methods for them

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,17 +16,27 @@ use App\Http\Controllers\Api\DataController;
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('allTeachers', [DataController::class, 'allTeachers'])->middleware('auth:sanctum');
         Route::get('getPerson/{person}', [DataController::class, 'getPerson'])->middleware('auth:sanctum');
+        Route::get('getStudent/{person}', [DataController::class, 'getStudentByPerson'])->middleware('auth:sanctum');
+
 
         Route::get('appointment/{appointment}', [DataController::class, 'getAppointmentById'])->middleware('auth:sanctum');
         Route::get('appointment/{student}/byStudent', [DataController::class, 'getAppointmentByStudentId'])->middleware('auth:sanctum');
         Route::get('appointment/{teacher}/byTeacher', [DataController::class, 'getAppointmentByTeacherId'])->middleware('auth:sanctum');
-        Route::get('appointment', [DataController::class, 'getAppointments'])->middleware('auth:sanctum');
-        Route::post('appointment/create', [DataController::class, 'makeAppointment'])->middleware('auth:sanctum');
+
+        Route::get('appointments', [DataController::class, 'getAppointments'])->middleware('auth:sanctum');
+        Route::get('appointments/week/{student}', [DataController::class, 'getAppointmentsByWeek'])->middleware('auth:sanctum');
+        Route::get('appointments/month/{student}', [DataController::class, 'getAppointmentsByMonth'])->middleware('auth:sanctum');
+        Route::get('appointments/day/{student}', [DataController::class, 'getAppointmentsByDay'])->middleware('auth:sanctum');
+        Route::get('appointments/inThreeDays/{student}', [DataController::class, 'getAppointmentsByThreeDays'])->middleware('auth:sanctum');
+
+        Route::post('appointment', [DataController::class, 'makeAppointment'])->middleware('auth:sanctum');
         Route::delete('appointment/{appointment}/delete', [DataController::class, 'deleteAppointment'])->middleware('auth:sanctum');
+
 
         Route::post('note/create', [DataController::class, 'createNote'])->middleware('auth:sanctum');
         Route::get('note/{student}/getNote', [DataController::class, 'getNotesByStudentId'])->middleware('auth:sanctum');
         Route::delete('note/{note}/deleteNote', [DataController::class, 'deleteNote'])->middleware('auth:sanctum');
+
 
         Route::get('reviews/{teacher}/byTeacher', [DataController::class, 'getReviewsByTeacher'])->middleware('auth:sanctum');
         Route::get('reviews/{student}/byStudent', [DataController::class, 'getReviewsByStudentId'])->middleware('auth:sanctum');
