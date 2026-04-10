a2fc9029227d5e1598e47cf35a4c139bc9223b13
--replace-all <mrakkus15@gmail.com>
2026-04-10 12:29:03 +0300

    feat: added account deletion functionality in ProfileComponent and API integration

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -8,19 +8,19 @@ import LessonsContainer from "@/shared/ui/LessonsContainer";
 export default function Dashboard() {
 
     return (
-        <div style={{ width: '100%', height: '100%', zIndex: -1}}>
+        <div style={{ width: '100%', height: '100%', zIndex: -10}}>
             <Threads
                 amplitude={1}
                 distance={0}
                 enableMouseInteraction
             />
-            <section className={`w-full h-[99vh] flex flex-col items-center justify-start gap-[5%] mt-[1%]`}>
+            <section className={`w-full h-[99vh] flex flex-col items-center justify-start gap-[5%] mt-[1%] z-0`}>
                 <div className={`flex flex-row items-center justify-between w-10/12`}>
                     <p className={`text-lg w-1/4 text-white`}>Tavs personals panelis, šeit tu vari redzēt visas stundas kas tevi gaida un pārvaldīt tos</p>
                     <DashboardButton role="student"/>
                 </div>
                 <div className={`flex flex-row items-center justify-between w-10/12`}>
-                    <ProfileComponent name={"Marks"} city={"Riga"} role={"Student"} surname={"Krustans"}/>
+                    <ProfileComponent/>
                     <LessonsCounter timePeriod={"today"}/>
                     <LessonsCounter timePeriod={"week"}/>
                 </div>
