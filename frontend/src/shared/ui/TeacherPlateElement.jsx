3241d8b35d91efeabacd0629c6c0518930607dfc
--replace-all <mrakkus15@gmail.com>
2026-04-30 12:59:54 +0300

    refactor: improved landing page responsiveness and layout consistency, updated TutorSection and TeacherPlateElement for better alignment and scaling

--- frontend/src/shared/ui/TeacherPlateElement.jsx
+++ frontend/src/shared/ui/TeacherPlateElement.jsx
@@ -12,11 +12,12 @@ export default function TeacherPlateElement({name, surname, subject}) {
             opacity={50}
             backgroundOpacity={0.001}
             mixBlendMode="difference"
-            width={`50%`}
-            height={`15%`}>
+            width={`100%`}
+            height={`40%`}
+            className="md:w-[50%]! md:h-[15%]!">
             <div className={`w-full h-full flex flex-row justify-between items-center p-4`}>
                 <div className={`flex flex-col items-start justify-center gap-2 w-[40%]`}>
-                    <p className={`font-bold text-xl text-white`}>{name} {surname}</p>
+                    <p className={`font-bold text-xl text-white w-full`}>{name} {surname}</p>
                     <p className={`text-lg font-thin text-white`}>{subject}</p>
                 </div>
                 <div className={`flex flex-col items-end justify-center gap-2 w-[40%]`}>
